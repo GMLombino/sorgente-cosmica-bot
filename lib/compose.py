c76c18da49e1f0174cab0942896f63d7ba5b578c
gml <lombino.g@libero.it>
2026-09-24 20:52:00 +0200

    Update compose.py

--- lib/compose.py
+++ lib/compose.py
@@ -1,7 +1,3 @@
-"""
-Composizione dell'immagine finale: sfondo generato dall'IA + frase + firma,
-disegnati con Pillow per un risultato tipografico controllato e coerente.
-"""
 import io
 from itertools import combinations
 import config
